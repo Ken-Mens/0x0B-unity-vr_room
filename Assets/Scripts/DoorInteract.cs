7feaa9811742b3c025beb8af1fe159e3508b35d7
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-25 17:04:54 -0700

    add script for interacting door

---
+++ Assets/Scripts/DoorInteract.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteract : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public Transform player;
+    public GameObject mini;
+
+    public void Interact()
+    {
+        if (InRange())
+        {
+            DoorTrigger();
+        }
+    }
+
+    bool InRange()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        if (dist <= 1f)
+        {
+            return (true);
+        }
+        return (false);
+    }
+
+    public void DoorTrigger()
+    {
+        doorAnimator.SetTrigger("character_nearby");
+    }
+}
