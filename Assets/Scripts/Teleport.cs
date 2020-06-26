b64692fa65d338fcdda461ec891bb9366b07d6d0
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-25 17:03:29 -0700

    script for teleporting around scene

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+  //  public GameObject player;
+  //
+   // public void PointerClick()
+    //{
+    //    player.transform.position = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
+   // }
+    public Transform player;
+    public Animator anim;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void TeleportMove()
+    {
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform != null)
+            {
+               anim.SetTrigger("Blink1");
+                player.position = hit.point;
+               anim.SetTrigger("Blink2");
+            }
+        }
+    }
+}
