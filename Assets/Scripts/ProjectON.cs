209d1e49f34b310abd7707fea35ded7e98202cd5
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-25 17:03:00 -0700

    script for turning on projector

---
+++ Assets/Scripts/ProjectON.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectON : MonoBehaviour
+{
+    public GameObject particles;
+    public Transform playerpos;
+
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        if (IsRange())
+        {
+            ParticlesON();
+        }
+        
+    }
+
+    bool IsRange()
+    {
+        float dist = Vector3.Distance(playerpos.position, transform.position);
+        if (dist <= 2f)
+        {
+            return (true);
+        }
+        return (false);
+    }
+
+  public void ParticlesON()
+    {
+        if (particles.activeInHierarchy == false)
+        {
+            particles.SetActive(true);
+        }
+    }
+}
